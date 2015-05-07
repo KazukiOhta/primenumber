875b1af66cf17c4b11bc7c4dd061de2a3233b674
太田　一毅 <kazukiota121@gmail.com>
2015-05-07 22:53:33 +0900

    Initial Commit

---
+++ vol.0_09/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  vol.0_09
+//
+//  Created by 太田　一毅 on 2015/05/07.
+//  Copyright (c) 2015年 太田一毅. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
