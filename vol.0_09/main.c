81e19f5defa563223769b4f1d8f0268ae2a51d79
太田　一毅 <kazukiota121@gmail.com>
2015-05-15 01:54:48 +0900

    First commit.

--- vol.0_09/main.c
+++ vol.0_09/main.c
@@ -7,9 +7,65 @@
 //
 
 #include <stdio.h>
+#include <math.h>
+int data[30];
+
+int n;
+int i;
+int l;
+int k;
+int m;
+int n;
+int o;
+int p;
+int max_data;
+int num_of_prime;
+int prime_num[10000];
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    for (i = 0; i < 30 ; i ++){
+        data[i] = -1;
+    }
+    
+    i = 0;
+    
+    while (1) {
+        scanf("%d",&data[i]);
+        if (data[i] == -1){
+            break;
+        }
+        i ++;
+    }
+    
+    for (n = 0; n < i; n ++) {
+        if (max_data < data[n]) {
+            max_data = data[n];
+        }
+    }
+    
+    for (m = 0; m < i; m ++) {
+        for (n = 2; n < max_data; n ++) {
+            l = 0;
+            for (k = 0; k < num_of_prime; k ++) {
+                if (n % prime_num[k] == 0) {
+                    l = 1;
+                }
+            }
+            if (l == 0) {
+                prime_num [num_of_prime] = n;
+                num_of_prime ++;
+            }
+        }
+//        printf("%d\n",num_of_prime);
+    }
+    
+    for (o = 0; o < i; o ++) {
+        while (prime_num[p] < data[o]) {
+            p ++;
+        }
+        printf("%d\n",p);
+    }
+    
     return 0;
 }
